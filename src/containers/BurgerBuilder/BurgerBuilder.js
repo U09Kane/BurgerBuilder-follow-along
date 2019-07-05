08d599eb939395f6ff3b85c674bdcbd985b91936
Joshua Maxcy <kanemaxcy@gmail.com>
2019-07-04 23:54:07 -0400

    added ability to click away from error message

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -136,4 +136,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default withErrorHandler(BurgerBuilder);
+export default withErrorHandler(BurgerBuilder, axios);
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -14,18 +14,25 @@ const withErrorHandler = (InnerComponent, axios) => {
             axios.interceptors.response.use(
                 req => {
                     this.setState({error: null})
+                    return req
             });
             axios.interceptors.response.use(
-                null, error => {
+                res => res, error => {
                     this.setState({error: error});
             });
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error: null})
         }
 
         render () {
             return (
                 <Aux>
-                    <Modal show={this.state.error}>
-                        {this.state.error.message}
+                    <Modal
+                        show={this.state.error}
+                        modalClosed={this.errorConfirmedHandler}>
+                        {this.state.error? this.state.error.message: null}
                     </Modal>
                     <InnerComponent {...this.props}/>
                 </Aux>
